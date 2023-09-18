0e7b9a81bc664e04cf8fdebe06c8fe71e3550514
Karan Chauhan <86901403+thekaranchauhan@users.noreply.github.com>
2023-09-18 09:51:39 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    distDir: 'build',
+    output:'export',
+  },
+};
+
+module.exports = nextConfig;
