ec131f9582cd209912d5f35e7c1b0236ebb38e9f
thekaranchauhan <chauhankaran2399@gmail.com>
2023-09-30 13:44:53 -0400

    updated form services

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,10 +1,49 @@
-'use client';
-import { type } from "os";
+"use client";
 import NewsLatterBox from "./NewsLatterBox";
-
+import emailjs from "@emailjs/browser";
+import React, { useState } from "react";
 
 
 const Contact = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Use your service ID, template ID, and user ID from EmailJS
+    const serviceID = "service_023wni6";
+    const templateID = "template_bbqqt9b";
+    const userID = "VRIU_kQD3N7ub85Fl";
+
+    // Get values from the form
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const contactNumber = e.target.contactNumber.value;
+    const message = e.target.message.value;
+
+    // Prepare the data to be sent
+    const templateParams = {
+      name,
+      email,
+      contactNumber,
+      message,
+    };
+
+    // Send the email
+    emailjs
+    .send(serviceID, templateID, templateParams, userID)
+    .then((response) => {
+      console.log("Email sent successfully:", response);
+      setIsSuccess(true);
+
+      // Reset the form fields
+      e.target.reset();
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+    });
+};
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
@@ -12,8 +51,7 @@ const Contact = () => {
           <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
               className="wow fadeInUp mb-12 rounded-md bg-primary/[3%] py-11 px-8 dark:bg-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-              data-wow-delay=".15s
-              "
+              data-wow-delay=".15s"
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Any Queries? Contact Us Now
@@ -21,7 +59,7 @@ const Contact = () => {
               <p className="mb-12 text-base font-medium text-body-color">
                 We will get back to you ASAP.
               </p>
-              <form>
+              <form onSubmit={sendEmail}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -33,8 +71,9 @@ const Contact = () => {
                       </label>
                       <input
                         type="text"
+                        name="name"
                         placeholder="Enter your name"
-                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp" 
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
                         required
                       />
                     </div>
@@ -49,8 +88,9 @@ const Contact = () => {
                       </label>
                       <input
                         type="email"
+                        name="email"
                         placeholder="Enter your email"
-                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp" 
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
                         required
                       />
                     </div>
@@ -58,15 +98,16 @@ const Contact = () => {
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
                       <label
-                        htmlFor="contact"
+                        htmlFor="contactNumber"
                         className="mb-3 block text-sm font-medium text-dark dark:text-white"
                       >
                         Your Contact Number
                       </label>
                       <input
                         type="tel"
+                        name="contactNumber"
                         placeholder="Enter contact number"
-                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp" 
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
                         required
                       />
                     </div>
@@ -88,12 +129,20 @@ const Contact = () => {
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+                    >
                       Submit
                     </button>
                   </div>
                 </div>
               </form>
+              {isSuccess && (
+                <div className="mt-4 text-green-600">
+                  Email sent successfully! We'll get back to you soon.
+                </div>
+              )}
             </div>
           </div>
           <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
