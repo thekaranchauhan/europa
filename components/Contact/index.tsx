5c95822ea246271226cb332d1fe5dc486b193eec
thekaranchauhan <chauhankaran2399@gmail.com>
2023-09-30 15:09:11 -0400

    updated form services

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -131,6 +131,7 @@ const Contact = () => {
                   <div className="w-full px-4">
                     <button
                       type="submit"
+                      value="send"
                       className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
                     >
                       Submit
